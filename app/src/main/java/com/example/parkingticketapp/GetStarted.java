6d2791d90555faa447aeb70ddbe8ab21f2307e27
Alex <52848335+mrash28@users.noreply.github.com>
2021-03-14 17:21:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/parkingticketapp/GetStarted.java
@@ -0,0 +1,26 @@
+package com.example.parkingticketapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GetStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+    }
+
+}
