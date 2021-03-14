c916aab153d0dadfce1adb647119a5250d4fc0fc
Alex <52848335+mrash28@users.noreply.github.com>
2021-03-15 02:22:28 +0530

    modified

--- app/src/main/java/com/example/parkingticketapp/ResetPassword.java
+++ app/src/main/java/com/example/parkingticketapp/ResetPassword.java
@@ -1,14 +1,82 @@
 package com.example.parkingticketapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPassword extends AppCompatActivity {
+
+    private TextView goToLogin;
+    private Button mResetBtn;
+    private EditText mEmail;
+    private ProgressBar progressBar;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        goToLogin = (TextView) findViewById(R.id.backToLogin);
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResetPassword.this, Login.class));
+            }
+        });
+
+        mEmail = (EditText) findViewById(R.id.email);
+        mResetBtn = (Button) findViewById(R.id.resetBtn);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mResetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+        });
+    }
+
+    private void resetPassword() {
+        String email = mEmail.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            mEmail.setError("Email is required");
+            mEmail.requestFocus();
+            return;
+        }
+
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("Enter a valid email address");
+            mEmail.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ResetPassword.this, "Check your mail to reset your password", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ResetPassword.this, "Oops.. something went wrong", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
